8ea93857075fbee9fe0b9cad5ae4c80d41eee09a
tom <tom@qq.com>
2024-08-16 14:18:58 +0800

    myip

--- server/monitor-types/shadowsocks.js
+++ server/monitor-types/shadowsocks.js
@@ -17,7 +17,8 @@ const { spawn } = require("node:child_process");
 // const ipUrl = 'https://cz88.net/api/cz88/ip/openIPInfo?ip='
 // const ipUrl = 'http://172.104.61.139/ip.php'
 // const ipUrl = 'http://cdn1.4cdn.xyz:9991/myip'
-const ipUrl = 'http://myip.ipip.net'
+// const ipUrl = 'http://myip.ipip.net'
+const ipUrl = 'https://api.vvhan.com/api/ipInfo'
 // const ipUrl = 'https://ipinfo.io/ip/'
 // monitor shadowsocks endpoint
 class ShadowsocksMonitorType extends MonitorType {
